b601363d9d6d3a291864a93cd4c3bd023e8126b0
VIGNESH M <vickymarimuthu0512@gmail.com>
2021-12-08 09:01:12 +0530

    [VIGNESH M] Added: AddressBook Class

---
+++ AddressBook.js
@@ -0,0 +1,24 @@
+//UC1-Create Address Book
+class AddressBook
+{
+    firstName;
+    lastName;
+    address;
+    city;
+    state;
+    zip;
+    phoneNumber;
+    email;
+
+    constructor(firstName,lastName,address,city,state,zip,phoneNumber,email)
+    {
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.address=address;
+        this.city=city;
+        this.state=state;
+        this.zip=zip;
+        this.phoneNumber=phoneNumber;
+        this.email=email;
+    }
+}
